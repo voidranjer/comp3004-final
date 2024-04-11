42927b3e0bbf289926492d32ca75dc8dbf5108aa
Aekus Singh <92745792+ayekus@users.noreply.github.com>
2024-04-11 15:19:04 -0400

    Added lights as well as power button with changing colours based on status of button

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,22 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    connect(ui->powerButton, SIGNAL(released()), this, SLOT (clicked()));
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::clicked()
+{
+    QPushButton* button = ui->powerButton;
+
+    if (button->palette().color(QPalette::ButtonText) == Qt::red) {
+        button->setStyleSheet("QPushButton {color: blue; border: none;}");
+    } else {
+        button->setStyleSheet("QPushButton {color: red; border: none;}");
+    }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QDebug>
+
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -16,6 +18,9 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+public slots:
+    void clicked();
 
 private:
     Ui::MainWindow *ui;
